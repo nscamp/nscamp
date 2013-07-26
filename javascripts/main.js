704096b359193fe2c2fd4eb2a7ef3c08df449eaf
Dougal MacPherson <dougal@jtribe.com.au>
2013-07-26 15:28:52 +1000

    Add js to highlight the current link in the menu

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,18 @@
-console.log('This would be the main JS file.');
+// 2013-07-26
+// Dougal MacPherson <dougal@jtribe.com.au>
+// 
+// Some js hacks to hightlight the current page
+
+$('#menu-main a').each(function(index) {
+  
+  // create a tmp link in memory and get the browser
+  // to parse the pathname, so we don't have to deal with 
+  // messy regexp to detect pathname
+  var tmp_ = document.createElement('a');
+      tmp_.href = this.href
+      
+      if (tmp_.pathname == document.location.pathname) {
+        return $(this).addClass('current');
+      }
+});
+
